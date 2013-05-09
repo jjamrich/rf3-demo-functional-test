d7cab70d049dcb9c529da66e7cd46d4c93785cdf
Ján Jamrich <jjamrich@redhat.com>
2013-05-09 13:38:17 +0200

    Add new abstract test class bringing Graphene and new Selenium into functional-test module

---
+++ src/test/java/org/jboss/richfaces/integrationTest/AbstractGrapheneTestCase.java
@@ -0,0 +1,52 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.richfaces.integrationTest;
+
+import java.io.File;
+import java.net.URL;
+
+import org.jboss.arquillian.ajocado.framework.GrapheneSelenium;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+/**
+ * @author <a href="jjamrich@redhat.com">Jan Jamrich</a>
+ *
+ */
+public class AbstractGrapheneTestCase extends Arquillian {
+
+    @Drone
+    protected GrapheneSelenium selenium;
+    
+    @ArquillianResource
+    protected URL contextPath;
+
+    @Deployment(testable = false)
+    public static WebArchive deployment() {
+        return ShrinkWrap.createFromZipFile(WebArchive.class, new File("target/richfaces-demo.war"));
+    }
+
+}
